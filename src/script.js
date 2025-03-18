a2dcea6cb793286710afa96e29872efe5cd4e19f
andreamiljak <andreamiljak52@gmail.com>
2025-03-18 22:56:50 +0100

    add plane for terrain

--- src/script.js
+++ src/script.js
@@ -1,10 +1,34 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import GUI from 'lil-gui'
 
+
+const gui = new GUI({width: 340})
 
 const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene()
 
+const debugObject = {
+    planeColor: '#57B257'
+}
+//Geometry
+const planeGeometry = new THREE.PlaneGeometry(32, 32, 128, 128)
+
+//Material
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: debugObject.planeColor
+})
+
+//Mesh
+const mesh = new THREE.Mesh(planeGeometry, planeMaterial)
+mesh.rotation.x -= Math.PI / 2
+scene.add(mesh)
+
+gui.addColor(debugObject, 'planeColor').name('Terrain Color').onChange(() =>
+{
+    planeMaterial.color.set(debugObject.planeColor)
+})
 
 //sizes
 const sizes = {
@@ -29,9 +53,11 @@ window.addEventListener('resize', () =>
     
 //Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 3, 5)
+camera.position.set(0, 3, 5)
 camera.lookAt(0, 0, 0)
 scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -43,3 +69,19 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//Animate
+
+const clock = new THREE.Clock()
+
+const tick = () => 
+{
+    controls.update()
+
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+
+}
+
+tick()
